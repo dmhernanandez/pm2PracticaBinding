09912965ec4ba147c2d4b411b519a8f3a1ec5a61
dmhernanandez <dany1999hernan@gmail.com>
2021-06-18 23:49:10 -0600

    Agregar archivos de proyecto.

---
+++ Practica binding/Practica binding/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica_binding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var nuevaPagina = new SegundPagina();
+            nuevaPagina.BindingContext = new {Nombre = TxtNombre.Text,
+            Apellido = TxtApellido.Text, Edad =TxtEdad.Text, Correo =TxtCorreo.Text};
+            await Navigation.PushAsync(nuevaPagina);
+            
+        }
+
+    }
+}
---
+++ Practica binding/Practica binding/SegundPagina.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica_binding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SegundPagina : ContentPage
+    {
+        public SegundPagina()
+        {
+            InitializeComponent();
+        }
+    }
+}
